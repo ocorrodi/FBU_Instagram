651920ef033d453fcd6f49c3036ca7513882f7d9
Olivia Corrodi <olc@fb.com>
2020-07-08 19:14:14 -0400

    added post model with retrieval from parse

--- app/src/main/java/com/example/fbu_instagram/MainActivity.java
+++ app/src/main/java/com/example/fbu_instagram/MainActivity.java
@@ -3,12 +3,51 @@ package com.example.fbu_instagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+
+    private EditText etDescription;
+    private Button btnUpload;
+    private Button btnSubmit;
+    private ImageView ivPostImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etDescription = findViewById(R.id.etDescription);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        btnUpload = findViewById(R.id.btnUpload);
+        ivPostImage = findViewById(R.id.ivPostImage);
+    }
+
+    private void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "issue with getting posts");
+                }
+                for (Post post : objects) {
+                    Log.i(TAG, "Post: " + post.getDescription() + " username: " + post.getUser().getUsername());
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/fbu_instagram/ParseApplication.java
+++ app/src/main/java/com/example/fbu_instagram/ParseApplication.java
@@ -2,12 +2,15 @@ package com.example.fbu_instagram;
 
 import android.app.Application;
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("fbu--parstagram") // should correspond to APP_ID env variable
