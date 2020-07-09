66b2bd2200e39bc8fda4cfad590da31a4d5a0091
Olivia Corrodi <olc@fb.com>
2020-07-09 13:54:52 -0400

    added bottom navigation logic

--- app/src/main/java/com/example/fbu_instagram/MainActivity.java
+++ app/src/main/java/com/example/fbu_instagram/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.fbu_instagram;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -11,6 +13,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -76,6 +79,26 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 launchCamera();
+            }
+        });
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_compose:
+                        // do something here
+                        break;
+                    case R.id.action_home:
+                        // do something here
+                         break;
+                    case R.id.action_profile:
+                        // do something here
+                        break;
+                    default: break;
+                }
+                return true;
             }
         });
     }
