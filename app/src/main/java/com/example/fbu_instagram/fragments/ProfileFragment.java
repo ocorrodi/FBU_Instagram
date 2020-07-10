c73720d4c130084eac5123dd7b07955370b46611
Olivia Corrodi <olc@fb.com>
2020-07-10 09:25:06 -0400

    refactored profile fragment into new fragment

--- app/src/main/java/com/example/fbu_instagram/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/fbu_instagram/fragments/ProfileFragment.java
@@ -1,21 +1,58 @@
 package com.example.fbu_instagram.fragments;
 
+import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fbu_instagram.Post;
+import com.example.fbu_instagram.PostsAdapter;
+import com.example.fbu_instagram.R;
 import com.example.fbu_instagram.fragments.PostsFragment;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.example.fbu_instagram.Post.KEY_CREATED_AT;
 
-public class ProfileFragment extends PostsFragment {
+public class ProfileFragment extends Fragment {
+
+    RecyclerView rvPosts;
+    public static final String TAG = "PostsFragment";
+    protected PostsAdapter adapter;
+    protected List<Post> allPosts;
+
+    public ProfileFragment() {}
 
     @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rvPosts = view.findViewById(R.id.rvPosts);
+        allPosts = new ArrayList<>();
+        adapter = new PostsAdapter(getContext(), allPosts);
+        rvPosts.setAdapter(adapter);
+        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+        queryPosts();
+    }
+
     protected void queryPosts() {
         ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
 
